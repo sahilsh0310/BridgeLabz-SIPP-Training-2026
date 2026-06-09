3c468589c7a19e5a3456aa487efd6de3de859a60
sahilsh0310 <sahil.sh0310@gmail.com>
2026-06-09 09:42:01 +0530

    [Sahil] Add: Added Programming Elements Assignment RaviCodingGuild

---
+++ core-java-practice/gcr-codebase/programming-elements/ravicodingguild.java
@@ -0,0 +1,17 @@
+public class ravicodingguild {
+    public static void main(String[] args) {
+        String name = "Ravi";
+        int age = 30;
+        int rank = 1;
+        int salary = 50000;
+        float Fees = 2000;
+        int annual_bonus = salary*12/100;
+        System.out.println("Name: " + name);
+        System.out.println("Ravi's Age: " + age);
+        System.out.println("Rank of Ravi: " + rank);
+        System.out.println("Salary of Ravi: " + salary);
+        System.out.println("Annual Bonus: " + annual_bonus);
+        System.out.println("Fees: " + Fees);
+        System.out.println("Welcome to Ravi!");
+    }
+}
